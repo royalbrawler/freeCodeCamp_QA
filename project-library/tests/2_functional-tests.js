532eac927d24d39ad64dcd27805986ff4b08fadd
royalbrawler <royalbrawler@users.noreply.github.com>
2025-12-06 13:33:20 +0200

    Update 2_functional-tests.js

--- project-library/tests/2_functional-tests.js
+++ project-library/tests/2_functional-tests.js
@@ -20,7 +20,7 @@ suite('Functional Tests', function () {
   /*
   * ----[EXAMPLE TEST]----
   * Each test should completely test the response of the API end-point including response status code!
-  */
+  *
   test('#example Test GET /api/books', function (done) {
     chai.request(server)
       .get('/api/books')
@@ -33,7 +33,7 @@ suite('Functional Tests', function () {
         done();
       });
   });
-  /*
+  *
   * ----[END of EXAMPLE TEST]----
   */
 
