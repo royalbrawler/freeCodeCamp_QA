fcf35bb943c17ed997586ca61f9aba4693f21f3b
royalbrawler <royalbrawler@users.noreply.github.com>
2025-12-12 13:12:02 +0200

    fix: typo fix

--- project-american-british-english-translator/tests/1_unit-tests.js
+++ project-american-british-english-translator/tests/1_unit-tests.js
@@ -51,48 +51,47 @@ suite('Unit Tests', () => {
         });
     });
     suite('British English To American English ', () => {
-        test('Translate We watched the footie match for a while. to British English', (done) => {
+        test('Translate We watched the footie match for a while. to American English', (done) => {
             assert.equal(translator.BritishToAmerican('We watched the footie match for a while.'), 'We watched the <span class="highlight">soccer</span> match for a while.');
             done();
         });
-        test('Translate Paracetamol takes up to an hour to work. to British English', (done) => {
+        test('Translate Paracetamol takes up to an hour to work. to American English', (done) => {
             assert.equal(translator.BritishToAmerican('Paracetamol takes up to an hour to work.'), '<span class="highlight">Tylenol</span> takes up to an hour to work.');
             done();
         });
-        test('Translate First, caramelise the onions. to British English', (done) => {
+        test('Translate First, caramelise the onions. to American English', (done) => {
             assert.equal(translator.BritishToAmerican('First, caramelise the onions.'), 'First, <span class="highlight">caramelize</span> the onions.');
             done();
         });
-        test('Translate I spent the bank holiday at the funfair. to British English', (done) => {
+        test('Translate I spent the bank holiday at the funfair. to American English', (done) => {
             assert.equal(translator.BritishToAmerican('I spent the bank holiday at the funfair.'), 'I spent the <span class="highlight">public holiday</span> at the <span class="highlight">carnival</span>.');
             done();
         });
-        test('Translate I had a bicky then went to the chippy. to British English', (done) => {
+        test('Translate I had a bicky then went to the chippy. to American English', (done) => {
             assert.equal(translator.BritishToAmerican('I had a bicky then went to the chippy.'), 'I had a <span class="highlight">cookie</span> then went to the <span class="highlight">fish-and-chip shop</span>.');
             done();
         });
 
-        test('Translate I\'ve just got bits and bobs in my bum bag. to British English', (done) => {
+        test('Translate I\'ve just got bits and bobs in my bum bag. to American English', (done) => {
             assert.equal(translator.BritishToAmerican("I've just got bits and bobs in my bum bag."), 'I\'ve just got <span class="highlight">odds and ends</span> in my <span class="highlight">fanny pack</span>.');
             done();
         });
-        test('Translate The car boot sale at Boxted Airfield was called off. to British English', (done) => {
+        test('Translate The car boot sale at Boxted Airfield was called off. to American English', (done) => {
             assert.equal(translator.BritishToAmerican('The car boot sale at Boxted Airfield was called off.'), 'The <span class="highlight">swap meet</span> at Boxted Airfield was called off.');
             done();
         });
-        test('Translate Have you met Mrs Kalyani? to British English', (done) => {
+        test('Translate Have you met Mrs Kalyani? to American English', (done) => {
             assert.equal(translator.BritishToAmerican('Have you met Mrs Kalyani?'), 'Have you met <span class="highlight">Mrs.</span> Kalyani?');
             done();
         });
-        test('Translate Prof Joyner of King\'s College, London. to British English', (done) => {
+        test('Translate Prof Joyner of King\'s College, London. to American English', (done) => {
             assert.equal(translator.BritishToAmerican('Prof Joyner of King\'s College, London.'), '<span class="highlight">Prof.</span> Joyner of King\'s College, London.');
             done();
         });
-        test('Translate Tea time is usually around 4 or 4.30. to British English', (done) => {
+        test('Translate Tea time is usually around 4 or 4.30. to American English', (done) => {
             assert.equal(translator.BritishToAmerican('Tea time is usually around 4 or 4.30.'), 'Tea time is usually around 4 or <span class="highlight">4.30</span>.');
             done();
         });
-
     });
 
     // this tests has no value just to pass fcc tests 
